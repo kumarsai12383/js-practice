80db4f9f03dedda07c41ccbb7bf182ae8e42d5fd
kumarsai12383 <kumarsai05072005@gmail.com>
2026-01-13 10:08:37 +0530

    test

---
+++ logic_building.js
@@ -0,0 +1,11 @@
+let array = [1,4,20,3,5,7,9,2,6,8,10];
+let max = Math.max(...array);
+console.log(max)
+let maxi = Math.max(1,4,20,3,5,7,9,2,6,8,10);
+let name = "kumar"
+
+
+console.log(typeof(12));
+let num = -12;
+num = Math.abs(num);
+console.log(num);
--- practiced_logics.js
+++ practiced_logics.js
@@ -1,39 +1,39 @@
-// //-------------------------------------------------------------------------------simple login form with OTP verification
-let userName1 = prompt("Enter your name:");
-let password = prompt("Enter your password:");
-if (userName1.length === 0 && password.length === 0) {
-  alert("Please enter valid credentials");
-} else if (
-  /[A-Za-z]/.test(password) &&
-  /[0-9]/.test(password) &&
-  userName1.length > 0 &&
-  password.length > 0
-) {
-  let OTP = Math.floor(Math.random() * 999999);
-  alert(`Your OTP is ${OTP}`);
-  let enterOtp = Number(prompt("Enter your OTP:"));
-  if (OTP === enterOtp) {
-    alert(`Welcome ${userName1} we are happy to see you again`);
-  } else {
-    alert("Invalid OTP, please try again");
-  }
-} else if (
-  !/[A-Za-z]/.test(password) ||
-  !/[0-9]/.test(password) ||
-  password.length <= 0
-) {
-  alert("Please enter a valid password with letters and numbers");
-}
+// // //-------------------------------------------------------------------------------simple login form with OTP verification
+// let userName1 = prompt("Enter your name:");
+// let password = prompt("Enter your password:");
+// if (userName1.length === 0 && password.length === 0) {
+//   alert("Please enter valid credentials");
+// } else if (
+//   /[A-Za-z]/.test(password) &&
+//   /[0-9]/.test(password) &&
+//   userName1.length > 0 &&
+//   password.length > 0
+// ) {
+//   let OTP = Math.floor(Math.random() * 999999);
+//   alert(`Your OTP is ${OTP}`);
+//   let enterOtp = Number(prompt("Enter your OTP:"));
+//   if (OTP === enterOtp) {
+//     alert(`Welcome ${userName1} we are happy to see you again`);
+//   } else {
+//     alert("Invalid OTP, please try again");
+//   }
+// } else if (
+//   !/[A-Za-z]/.test(password) ||
+//   !/[0-9]/.test(password) ||
+//   password.length <= 0
+// ) {
+//   alert("Please enter a valid password with letters and numbers");
+// }
 
-//----------------------------------------------------------------------------------------------// maskEmail function
-function maskEmail(email) {
-  let atindex =
-    email[0] +
-    "*".repeat(email.slice(email[1], email.indexOf("@") - 1).length - 1) +
-    email[email.indexOf("@") - 1] +
-    email.slice(email.indexOf("@"));
-  return atindex;
-}
+// //----------------------------------------------------------------------------------------------// maskEmail function
+// function maskEmail(email) {
+//   let atindex =
+//     email[0] +
+//     "*".repeat(email.slice(email[1], email.indexOf("@") - 1).length - 1) +
+//     email[email.indexOf("@") - 1] +
+//     email.slice(email.indexOf("@"));
+//   return atindex;
+// }
 // let email = "Kumarsai@gmail.com";
 // console.log(maskEmail(email));
 // console.log(maskEmail("apple.pie@example.com"));
@@ -168,3 +168,25 @@ function maskEmail(email) {
 //   alert("Invalid DOB");
 // }
 
+//-------------------------------------------------------Vowels and consonants counter
+// function getVowelCount(str) {
+//   const vowels = 'aeiou';
+//   let vowelscount = 0;
+//   let consonantsCount = 0;
+//   for (let char of str.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       vowelscount++;
+//     }
+//     else if(!vowels.includes(char) && char >="a" && char <= "z"){
+//       consonantsCount++;
+//     }
+//   }
+//   let count = {
+//     vowels: vowelscount,
+//     consonants: consonantsCount
+//   }
+//   return count;
+// }
+// console.log(getVowelCount("Hello World")); 
+
+// console.log(getVowelCount("Programming is fun"));
