3a53c86d28d2b89790339ff72c604f8961289a1c
kumarsai12383 <kumarsai05072005@gmail.com>
2026-07-15 18:23:08 +0530

    just learing purpose

--- revision.js
+++ revision.js
@@ -1,25 +1,12 @@
-let items = [
-  {
-    id: 1,
-    name: "Item 1",
-    category: "Category A",
-    description: "This is the first item.",
-  },
-  {
-    id: 2,
-    name: "Item 2",
-    category: "Category B",
-    description: "This is the second item.",
-  },
-  {
-    id: 3,
-    name: "Item 3",
-    category: "Category A",
-    description: "This is the third item.",
-  },
-];
-
-let categoryA = items.find((item) => {
-  return item.category === "Category A";
-});
-console.log(categoryA);
+let first_number = 80;
+let second_number = 20;
+let third_number = 100;
+if (first_number > second_number && first_number > third_number) {
+  console.log("The first number is the largest.");
+}
+if (second_number > first_number && second_number > third_number) {
+  console.log("The second number is the largest.");
+}
+if (third_number > first_number && third_number > second_number) {
+  console.log("The third number is the largest.");
+}
